a6599a5518055f38dd64d7ef45cbf5e8a356060b
Meowmir <disewji@gmail.com>
2024-04-19 13:14:07 +0200

    working api with three different kinds of results

--- src/components/word-grid.component.tsx
+++ src/components/word-grid.component.tsx
@@ -1,25 +1,18 @@
 import {Grid} from "@mui/material";
+import GridItem from "./grid-item.tsx";
 
-export default function WordGrid({words} : {words: string[]}){
+export default function WordGrid({words} : {words: string[][]}){
   return (
     <>
      <Grid container>
-       <Grid item xs={6}  sx={{borderStyle: "solid", borderColor: "white"}}>
-         <p>Similar meaning to:</p>
-         <hr/>
-         {words.map((word) => (
-           <Grid item xs={6} key={word} sx={{ justifyContent: "center", alignItems: "center"}}>
-             <p>{word}</p>
-           </Grid>
-         ))}
+       <Grid item xs={4} sx={{padding: 2}}>
+         <GridItem title="Similar to:" words={words} index={0}/>
        </Grid>
-       <Grid item xs={6}>
-         <p>Similar meaning to:</p>
-         {words.map((word) => (
-           <Grid item xs={6} key={word} sx={{borderStyle: "solid", borderColor: "white"}}>
-             <p>{word}</p>
-           </Grid>
-         ))}
+       <Grid item xs={4} sx={{padding: 2}}>
+         <GridItem title="Sounds like:" words={words} index={1}/>
+       </Grid>
+       <Grid item xs={4} sx={{padding: 2}}>
+         <GridItem title="Words that often follow:" words={words} index={2}/>
        </Grid>
      </Grid>
     </>
--- src/views/front.view.tsx
+++ src/views/front.view.tsx
@@ -4,14 +4,14 @@ import Textfield from "../components/textfield.tsx";
 import getResults from "../utils/get-results.util.ts";
 
 export default function FrontView(){
-  const [words, setWord] = useState("")
-  const results = getResults(words)
+  const [userInput, setUserInput] = useState("")
+  const results = getResults(userInput)
 
   return (
     <>
       <h1>Words</h1>
       <h4>Start typing a word or a sentence</h4>
-      <Textfield onChange={setWord}/>
+      <Textfield onChange={setUserInput}/>
       <WordGrid
         words={results}
         />
